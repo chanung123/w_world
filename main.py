d13f968e49f6cada57b72a23ba0e0ddbcae47f1d
chanung123 <chanung123@gmail.com>
2023-05-24 17:15:50 +0900

    디자이너 고용해

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 # pylint: disable=C0114
 import sys
 import pygame
-import random
-import spritesheet
-
 
 
 class Room:
@@ -14,6 +11,7 @@ class Room:
         self.y = y
         self.status = "saferoom"
         self.view = False
+        
 
 
 class Player:
@@ -57,47 +55,6 @@ pygame.init()
 ## 초당 프레임 단위 설정 ##
 FPS = 60
 Clock = pygame.time.Clock()
-pygame.key.set_repeat(300, 1)
-FPSCLOCK = pygame.time.Clock()
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Spritesheets")
-
-sprite_sheet_image = pygame.image.load("girl.png").convert_alpha()
-sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
-
-
-BG = (0,0,0)
-BLACK = (0,0,0)
-WHITE = (255,255,255)
-WIDTH = 59.99
-HEIGHT = 120
-
-X=250
-Y=250
-
-
-
-#create animation list
-animation_list = []
-animation_steps = [8, 8, 8, 8]
-action = 0
-last_update = pygame.time.get_ticks()
-animation_cooldown = 100
-frame = 0
-step_counter = 0
-
-for animation in animation_steps:
-    temp_img_list = []
-    for _ in range(animation):
-        temp_img_list.append(sprite_sheet.get_image(step_counter, WIDTH, HEIGHT,2,BLACK))
-        step_counter += 1
-    animation_list.append(temp_img_list)
-
-print(animation_list)
 
 ## 컬러 세팅 ##
 BLUE = (0, 0, 255)
@@ -111,6 +68,61 @@ EDGE = 0  # (0,0)
 BOXSCALE = 130
 MARGIN = 100
 FIREPOSITION = 35
+FRAMSCALE = 265
+MOVE = False
+CLICK = False
+
+MX = 0
+MY = 0
+COLUMN_COUNT = 4
+ROW_COUNT = 4
+MOUS_X = 0
+MOUS_Y = 0
+
+column_index1 = 0
+row_index1 = 0
+column_index2 = 0
+row_index2 = 0
+
+frame_img = pygame.image.load("assets/frame.png")
+frame_img = pygame.transform.scale(frame_img, (FRAMSCALE, FRAMSCALE))
+frame_img_Rect = frame_img.get_rect()
+
+#마우스 격자
+def mouse_pos_x(pos_x):
+    if pos_x >= MARGIN and pos_x < MARGIN + BOXSCALE:
+        return 0
+    if pos_x >= MARGIN + BOXSCALE and pos_x < MARGIN + (BOXSCALE*2):
+        return 1
+    if pos_x >= MARGIN + (BOXSCALE*2) and pos_x < MARGIN + (BOXSCALE*3):
+        return 2
+    if pos_x >= MARGIN + (BOXSCALE*3) and pos_x < MARGIN + (BOXSCALE*4):
+        return 3
+    else:
+        return 0
+
+def mouse_pos_y(pos_y):
+    if pos_y >= MARGIN and pos_y < MARGIN + BOXSCALE:
+        return 0
+    if pos_y >= MARGIN + BOXSCALE and pos_y < MARGIN + (BOXSCALE*2):
+        return 1
+    if pos_y >= MARGIN + (BOXSCALE*2) and pos_y < MARGIN + (BOXSCALE*3):
+        return 2
+    if pos_y >= MARGIN + (BOXSCALE*3) and pos_y < MARGIN + (BOXSCALE*4):
+        return 3
+    else:
+        return 0
+    
+
+def M_icon(click):
+    if click == 0:
+        # 마우스 커서 기본
+        pygame.mouse.set_cursor(*pygame.cursors.arrow)
+    if click == 1:
+        # x표시
+        pygame.mouse.set_cursor(*pygame.cursors.diamond)
+        
+
 
 ## 게임 창 설정 ##
 screen = pygame.display.set_mode((1300, 720))
@@ -138,6 +150,32 @@ pit_img = renderimg("assets/pitch_rava.png")
 player_img = renderimg("assets/player.png")
 dark_img = renderimg("assets/dark.png")
 
+
+#격자 스케일(이미지 정렬)
+def point_core(postionx, postiony, box_scale, scale):
+    #중심을 기준으로 포지션
+    if box_scale < scale:
+        if postionx >= 0 and postionx <= 3:
+            if postiony >= 0 and postiony <= 3:
+                return ((postionx * BOXSCALE) + MARGIN - (scale-box_scale)/2, ((postiony) * BOXSCALE) + MARGIN -(scale-box_scale)/2)
+
+        if postionx > 3 or postiony > 3:
+            return ((postionx * BOXSCALE) + MARGIN - (scale-box_scale)/2, ((postiony-1) * BOXSCALE) + MARGIN - (scale-box_scale)/2)
+    if box_scale == scale:
+        if postionx >= 0 and postionx <= 3:
+            if postiony >= 0 and postiony <= 3:
+                return ((postionx * BOXSCALE) + MARGIN, ((postiony) * BOXSCALE) + MARGIN)
+
+        if postionx > 3 or postiony > 3:
+            return (((postionx) * BOXSCALE) + MARGIN, ((postiony - 1) * BOXSCALE) + MARGIN)
+    if box_scale > scale:
+        if postionx >= 0 and postionx <= 3:
+            if postiony >= 0 and postiony <= 3:
+                return ((postionx * BOXSCALE) + MARGIN + (box_scale-scale)/2, ((postiony) * BOXSCALE) + MARGIN + (box_scale-scale)/2)
+
+        if postionx > 3 or postiony > 3:
+            return ((postionx * BOXSCALE) + MARGIN + (box_scale-scale)/2, ((postiony-1) * BOXSCALE) + MARGIN + (box_scale-scale)/2)
+            
 
 def point(postionx, postiony):
     """플레이어 포지션 정하기"""
@@ -168,8 +206,8 @@ def textoutput(outtext):
 
 # 룸생성
 rooms = [[], [], [], []]
-for i in range(4):
-    for j in range(4):
+for i in range(COLUMN_COUNT):
+    for j in range(ROW_COUNT):
         rooms[i].append(Room(i, j))
 
 # 0,0초기화
@@ -179,39 +217,9 @@ rooms[0][0].view = True
 player = Player(0, 0)
 
 # 장애물 설정
-def random_num(r):
-    R = random.randint(0,r)
-    return R
-
-sample = 100 #표본개수
-
-for x in range(4):
-    for y in range(4):
-        if x == 0 and y == 0:
-            rooms[x][y].status = "none"
-        elif x == 3 and y == 3:
-            rooms[x][y].status = "gold"
-        else:
-            i = random_num(sample)
-            if i <= 0.1*sample:
-                rooms[x][y].status = "wumpus"
-            elif i > 0.1*sample and i <= 0.15*sample:
-                rooms[x][y].status = "pit"
-            elif i > 0.15*sample and i <= 0.2*sample:
-                rooms[x][y].status = "pit2"
-            else:
-                rooms[x][y].status = "none"
-
-
-
-
-    
-        # rooms[2][2].status = "wumpus"
-        # rooms[0][1].status = "wumpus"
-        # rooms[2][3].status = "pit"
-        # rooms[3][1].status = "pit"
-        # rooms[1][0].status = "pit2"
-        # rooms[3][3].status = "gold"
+rooms[2][2].status = "wumpus"
+rooms[2][3].status = "pit"
+rooms[3][3].status = "gold"
 
 
 def RenderMap(
@@ -227,38 +235,20 @@ def RenderMap(
     screen.blit(map_img, (24, 10))
     screen.blit(fire_img, (BOXSCALE + FIREPOSITION, 0))
     screen.blit(fire_img, ((3 * BOXSCALE) + FIREPOSITION, 0))
-    screen.blit(
-        fire_img_down, (BOXSCALE + FIREPOSITION, BOXSCALE * 4 + FIREPOSITION * 2)
-    )
+    screen.blit(fire_img_down, (BOXSCALE + FIREPOSITION, BOXSCALE * 4 + FIREPOSITION * 2))
     screen.blit(fire_img_down, ((3 * BOXSCALE) + 35, BOXSCALE * 4 + FIREPOSITION * 2))
     screen.blit(fire_img_left, (0, BOXSCALE + FIREPOSITION))
     screen.blit(fire_img_left, (0, (3 * BOXSCALE) + FIREPOSITION))
-    screen.blit(
-        fire_img_right, ((4 * BOXSCALE) + FIREPOSITION * 2, BOXSCALE + FIREPOSITION)
-    )
-    screen.blit(
-        fire_img_right,
-        ((4 * BOXSCALE) + FIREPOSITION * 2, (3 * BOXSCALE) + FIREPOSITION),
-    )
+    screen.blit(fire_img_right, ((4 * BOXSCALE) + FIREPOSITION * 2, BOXSCALE + FIREPOSITION))
+    screen.blit(fire_img_right,((4 * BOXSCALE) + FIREPOSITION * 2, (3 * BOXSCALE) + FIREPOSITION),)
 
 
 while True:
     Clock.tick(FPS)
-    
-    #update animation
-    current_time = pygame.time.get_ticks()
-    if current_time - last_update >= animation_cooldown:
-        frame += 1
-        last_update = current_time
-        if frame >= len(animation_list[action]):
-            frame = 0
-
-    
-
     # 현재위치
     currentRoom = rooms[player.x][player.y]
 
-
+    
     for event in pygame.event.get():
         # # 게임을 종료시키는 함수
         if event.type == pygame.QUIT:
@@ -269,17 +259,67 @@ while True:
                 sys.exit()
             if event.key == pygame.K_RIGHT and player.x < 3:
                 player.x += 1
-                action = 2
             if event.key == pygame.K_LEFT and player.x > 0:
                 player.x -= 1
-                action = 1
             if event.key == pygame.K_UP and player.y > 0:
                 player.y -= 1
-                action = 3
             if event.key == pygame.K_DOWN and player.y < 3:
                 player.y += 1
-                action = 0
             rooms[player.x][player.y].view = True
+
+            # 마우스 버튼이 눌렸을 때
+        if event.type == pygame.MOUSEBUTTONDOWN:
+                x1, y1 = event.pos
+
+                column_index1 = mouse_pos_x(x1)
+                row_index1 = mouse_pos_y(y1)
+
+                rooms[column_index1][row_index1].view =True
+
+                screen.blit(frame_img, (point_core(column_index1, row_index1, BOXSCALE, FRAMSCALE)))
+
+                CLICK = not CLICK
+                MOVE = True
+            
+                M_icon(CLICK)
+               
+                player.x = column_index1
+                player.y = row_index1
+
+                
+
+            # if CLICK == 0:
+            #     x2, y2 = event.pos
+
+            #     column_index2 = mouse_pos_x(x2)
+            #     row_index2 = mouse_pos_y(y2)
+
+            #     screen.blit(frame_img, (point_core(column_index2, row_index2, BOXSCALE, FRAMSCALE)))
+
+            #     CLICK = not CLICK
+            #     MOVE = True
+            
+            #     M_icon(CLICK)
+                
+            #     player.x = column_index2
+            #     player.y = row_index2
+
+
+
+        # 마우스 버튼이 올라갔을 때
+        if event.type == pygame.MOUSEBUTTONUP:
+        # Image가 이동하면 안되므로 MOVE는 False로
+            MOVE = False
+
+            # 마우스 커서의 모양을 기본값인 화살표 모양으로 변경
+            M_icon(CLICK)
+            # pygame.mouse.set_cursor(*pygame.cursors.arrow)
+
+
+           
+
+        
+                                    
 
     screen.fill(BLACK)
 
@@ -300,26 +340,40 @@ while True:
             if rooms[x][y].status == "wumpus":
                 screen.blit(wumpus_img, (point(x, y)))
             elif rooms[x][y].status == "pit":
-                screen.blit(pit_img, (point(x, y)))
-            elif rooms[x][y].status == "pit2":
-                screen.blit(pit_img2, (point(x, y)))
+                screen.blit(pit_img, (point(2, 3)))
             elif rooms[x][y].status == "gold":
-                screen.blit(gold_img, (point(x, y)))
+                screen.blit(gold_img, (point(3, 3)))
             # 지나간곳만 보임 (view가 false일떄)
             if not rooms[x][y].view:
                 screen.blit(dark_img, (point(x, y)))
+              
+
+    
+    screen.blit(frame_img, (point_core(column_index1, row_index1, BOXSCALE, FRAMSCALE)))
+
+    if CLICK:
+        # 플레이어 렌더링
+        screen.blit(player_img, (point(player.x, player.y)))
+
+        
+    
     # 플레이어 렌더링
-    # screen.blit(player_img, ))
+    screen.blit(player_img, (point(player.x, player.y)))
 
-    #show frame image
-    screen.blit(animation_list[action][frame], (point(player.x, player.y)))
+    x1, y1 = pygame.mouse.get_pos()
+    column_index1 = mouse_pos_x(x1)
+    row_index1 = mouse_pos_y(y1)
+    screen.blit(frame_img, (point_core(column_index1, row_index1, BOXSCALE, FRAMSCALE)))
 
-    screen.blit(
-        font.render(str(player.x) + "," + str(player.y), True, text_color),
-        (800, 100),
-    )
+
+    screen.blit(font.render(str(player.x) + "," + str(player.y), True, text_color),(800, 100))
+
+    
     for text in textArr:
         x = 30 * (textArr.index(text) + 1)
         screen.blit(text, (800, x + 100))
+
+
+    
 
     pygame.display.update()
