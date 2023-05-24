a895c850e237f998b81031d23824497ffcb172f2
songchez <tama4840@gmail.com>
2023-05-25 00:13:14 +0900

    정리

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 from classes.Player import Player
 from classes.Room import Room
+from position import BOXSCALE, RenderMap, mouse_pos_x, mouse_pos_y, point, point_core
 
 # Initialize the game engine
 # pylint: disable=no-member
@@ -13,26 +14,23 @@ FPS = 60
 Clock = pygame.time.Clock()
 
 ## 컬러 세팅 ##
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 # 격자만들기(가로)
 EDGE = 0  # (0,0)
-BOXSCALE = 130
-MARGIN = 100
+
 FIREPOSITION = 35
 FRAMSCALE = 265
 
-def M_icon(click):
-    if click == 0:
-        # 마우스 커서 기본
-        pygame.mouse.set_cursor(*pygame.cursors.arrow)
-    if click == 1:
-        # x표시
+def cursoricon(cursor):
+    """칸에 커서올라가면 마우스 변경"""
+    if cursor:
+        # 마우스 커서 on
         pygame.mouse.set_cursor(*pygame.cursors.diamond)
+    else:
+        # 마우스 커서 off
+        pygame.mouse.set_cursor(*pygame.cursors.arrow)
 
 
 ## 게임 창 설정 ##
@@ -63,71 +61,9 @@ player_img = renderimg("assets/player.png")
 dark_img = renderimg("assets/dark.png")
 
 
-#격자 스케일(이미지 정렬)
-def point_core(postionx, postiony, box_scale, scale):
-    #중심을 기준으로 포지션
-    if box_scale < scale:
-        if postionx >= 0 and postionx <= 3:
-            if postiony >= 0 and postiony <= 3:
-                return ((postionx * BOXSCALE) + MARGIN - (scale-box_scale)/2, ((postiony) * BOXSCALE) + MARGIN -(scale-box_scale)/2)
-
-        if postionx > 3 or postiony > 3:
-            return ((postionx * BOXSCALE) + MARGIN - (scale-box_scale)/2, ((postiony-1) * BOXSCALE) + MARGIN - (scale-box_scale)/2)
-    if box_scale == scale:
-        if postionx >= 0 and postionx <= 3:
-            if postiony >= 0 and postiony <= 3:
-                return ((postionx * BOXSCALE) + MARGIN, ((postiony) * BOXSCALE) + MARGIN)
-
-        if postionx > 3 or postiony > 3:
-            return (((postionx) * BOXSCALE) + MARGIN, ((postiony - 1) * BOXSCALE) + MARGIN)
-    if box_scale > scale:
-        if postionx >= 0 and postionx <= 3:
-            if postiony >= 0 and postiony <= 3:
-                return ((postionx * BOXSCALE) + MARGIN + (box_scale-scale)/2, ((postiony) * BOXSCALE) + MARGIN + (box_scale-scale)/2)
-
-        if postionx > 3 or postiony > 3:
-            return ((postionx * BOXSCALE) + MARGIN + (box_scale-scale)/2, ((postiony-1) * BOXSCALE) + MARGIN + (box_scale-scale)/2)
-            
-
-#마우스 격자
-def mouse_pos_x(pos_x):
-    if pos_x >= MARGIN and pos_x < MARGIN + BOXSCALE:
-        return 0
-    if pos_x >= MARGIN + BOXSCALE and pos_x < MARGIN + (BOXSCALE*2):
-        return 1
-    if pos_x >= MARGIN + (BOXSCALE*2) and pos_x < MARGIN + (BOXSCALE*3):
-        return 2
-    if pos_x >= MARGIN + (BOXSCALE*3) and pos_x < MARGIN + (BOXSCALE*4):
-        return 3
-    else:
-        return 0
-
-def mouse_pos_y(pos_y):
-    if pos_y >= MARGIN and pos_y < MARGIN + BOXSCALE:
-        return 0
-    if pos_y >= MARGIN + BOXSCALE and pos_y < MARGIN + (BOXSCALE*2):
-        return 1
-    if pos_y >= MARGIN + (BOXSCALE*2) and pos_y < MARGIN + (BOXSCALE*3):
-        return 2
-    if pos_y >= MARGIN + (BOXSCALE*3) and pos_y < MARGIN + (BOXSCALE*4):
-        return 3
-    else:
-        return 0
-
-def point(postionx, postiony):
-    """플레이어 포지션 정하기"""
-    if postionx >= 0 and postionx <= 3:
-        if postiony >= 0 and postiony <= 3:
-            return ((postionx * BOXSCALE) + MARGIN, ((postiony) * BOXSCALE) + MARGIN)
-
-    if postionx > 3 or postiony > 3:
-        return (((postionx) * BOXSCALE) + MARGIN, ((postiony - 1) * BOXSCALE) + MARGIN)
-
-
 text_color = WHITE  # Black
 font = pygame.font.Font("uhBeePuding.ttf", 28)
 textArr = []
-
 def textoutput(outtext):
     """텍스트 출력해주는 함수"""
     text_surface = font.render(outtext, True, text_color)
@@ -155,32 +91,10 @@ rooms[2][3].status = "pit"
 rooms[3][3].status = "gold"
 
 
-def RenderMap(
-    BOXSCALE,
-    FIREPOSITION,
-    screen,
-    map_img,
-    fire_img,
-    fire_img_down,
-    fire_img_left,
-    fire_img_right,
-):
-    screen.blit(map_img, (24, 10))
-    screen.blit(fire_img, (BOXSCALE + FIREPOSITION, 0))
-    screen.blit(fire_img, ((3 * BOXSCALE) + FIREPOSITION, 0))
-    screen.blit(fire_img_down, (BOXSCALE + FIREPOSITION, BOXSCALE * 4 + FIREPOSITION * 2))
-    screen.blit(fire_img_down, ((3 * BOXSCALE) + 35, BOXSCALE * 4 + FIREPOSITION * 2))
-    screen.blit(fire_img_left, (0, BOXSCALE + FIREPOSITION))
-    screen.blit(fire_img_left, (0, (3 * BOXSCALE) + FIREPOSITION))
-    screen.blit(fire_img_right, ((4 * BOXSCALE) + FIREPOSITION * 2, BOXSCALE + FIREPOSITION))
-    screen.blit(fire_img_right,((4 * BOXSCALE) + FIREPOSITION * 2, (3 * BOXSCALE) + FIREPOSITION),)
-
-
 while True:
     Clock.tick(FPS)
     # 현재위치
     currentRoom = rooms[player.x][player.y]
-
     for event in pygame.event.get():
         # # 게임을 종료시키는 함수
         if event.type == pygame.QUIT:
@@ -196,7 +110,6 @@ while True:
                 player.y = y
                 textoutput("마우스 이동")
                 rooms[player.x][player.y].view = True
-
 
 
     screen.fill(BLACK)
@@ -224,7 +137,7 @@ while True:
             # 지나간곳만 보임 (view가 false일떄)
             if not rooms[x][y].view:
                 screen.blit(dark_img, (point(x, y)))
-
+    
     #이동할 수 있는 곳 밝은 프레임으로 감싸기
     framePos = [-1,0],[1,0],[0,-1],[0,1]
     for pos_box in framePos:
@@ -243,8 +156,5 @@ while True:
     for text in textArr:
         x = 30 * (textArr.index(text) + 1)
         screen.blit(text, (800, x + 100))
-
-
-    
 
     pygame.display.update()
---
+++ position.py
@@ -0,0 +1,80 @@
+
+BOXSCALE = 130
+MARGIN = 100
+
+#격자 스케일(이미지 정렬)
+def point_core(postionx, postiony, box_scale, scale):
+    #중심을 기준으로 포지션
+    if box_scale < scale:
+        if postionx >= 0 and postionx <= 3:
+            if postiony >= 0 and postiony <= 3:
+                return ((postionx * BOXSCALE) + MARGIN - (scale-box_scale)/2, ((postiony) * BOXSCALE) + MARGIN -(scale-box_scale)/2)
+        if postionx > 3 or postiony > 3:
+            return ((postionx * BOXSCALE) + MARGIN - (scale-box_scale)/2, ((postiony-1) * BOXSCALE) + MARGIN - (scale-box_scale)/2)
+    if box_scale == scale:
+        if postionx >= 0 and postionx <= 3:
+            if postiony >= 0 and postiony <= 3:
+                return ((postionx * BOXSCALE) + MARGIN, ((postiony) * BOXSCALE) + MARGIN)
+        if postionx > 3 or postiony > 3:
+            return (((postionx) * BOXSCALE) + MARGIN, ((postiony - 1) * BOXSCALE) + MARGIN)
+    if box_scale > scale:
+        if postionx >= 0 and postionx <= 3:
+            if postiony >= 0 and postiony <= 3:
+                return ((postionx * BOXSCALE) + MARGIN + (box_scale-scale)/2, ((postiony) * BOXSCALE) + MARGIN + (box_scale-scale)/2)
+        if postionx > 3 or postiony > 3:
+            return ((postionx * BOXSCALE) + MARGIN + (box_scale-scale)/2, ((postiony-1) * BOXSCALE) + MARGIN + (box_scale-scale)/2)
+
+#마우스 격자
+def mouse_pos_x(pos_x):
+    if pos_x >= MARGIN and pos_x < MARGIN + BOXSCALE:
+        return 0
+    if pos_x >= MARGIN + BOXSCALE and pos_x < MARGIN + (BOXSCALE*2):
+        return 1
+    if pos_x >= MARGIN + (BOXSCALE*2) and pos_x < MARGIN + (BOXSCALE*3):
+        return 2
+    if pos_x >= MARGIN + (BOXSCALE*3) and pos_x < MARGIN + (BOXSCALE*4):
+        return 3
+    else:
+        return 0
+
+def mouse_pos_y(pos_y):
+    if pos_y >= MARGIN and pos_y < MARGIN + BOXSCALE:
+        return 0
+    if pos_y >= MARGIN + BOXSCALE and pos_y < MARGIN + (BOXSCALE*2):
+        return 1
+    if pos_y >= MARGIN + (BOXSCALE*2) and pos_y < MARGIN + (BOXSCALE*3):
+        return 2
+    if pos_y >= MARGIN + (BOXSCALE*3) and pos_y < MARGIN + (BOXSCALE*4):
+        return 3
+    else:
+        return 0
+
+def point(postionx, postiony):
+    """플레이어 포지션 정하기"""
+    if postionx >= 0 and postionx <= 3:
+        if postiony >= 0 and postiony <= 3:
+            return ((postionx * BOXSCALE) + MARGIN, ((postiony) * BOXSCALE) + MARGIN)
+
+    if postionx > 3 or postiony > 3:
+        return (((postionx) * BOXSCALE) + MARGIN, ((postiony - 1) * BOXSCALE) + MARGIN)
+
+def RenderMap(
+    BOXSCALE,
+    FIREPOSITION,
+    screen,
+    map_img,
+    fire_img,
+    fire_img_down,
+    fire_img_left,
+    fire_img_right,
+):
+    screen.blit(map_img, (24, 10))
+    screen.blit(fire_img, (BOXSCALE + FIREPOSITION, 0))
+    screen.blit(fire_img, ((3 * BOXSCALE) + FIREPOSITION, 0))
+    screen.blit(fire_img_down, (BOXSCALE + FIREPOSITION, BOXSCALE * 4 + FIREPOSITION * 2))
+    screen.blit(fire_img_down, ((3 * BOXSCALE) + 35, BOXSCALE * 4 + FIREPOSITION * 2))
+    screen.blit(fire_img_left, (0, BOXSCALE + FIREPOSITION))
+    screen.blit(fire_img_left, (0, (3 * BOXSCALE) + FIREPOSITION))
+    screen.blit(fire_img_right, ((4 * BOXSCALE) + FIREPOSITION * 2, BOXSCALE + FIREPOSITION))
+    screen.blit(fire_img_right,((4 * BOXSCALE) + FIREPOSITION * 2, (3 * BOXSCALE) + FIREPOSITION),)
+
